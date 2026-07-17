bac16a8836fe0e0a1a79b581b9cfcb615254bbdb
Angel Larios <alarios@casystem.com.mx>
2026-07-17 11:47:35 -0600

    agrega configraiocn inicial de mi proyecto

---
+++ customer_update/src/main/java/com/nova/customer_update/CustomerUpdateApplication.java
@@ -0,0 +1,13 @@
+package com.nova.customer_update;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerUpdateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerUpdateApplication.class, args);
+	}
+
+}
